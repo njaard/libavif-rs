7d6fe401dc122309f0b276c4200ace6ca883b509
Paolo Barbolini <paolo@paolo565.org>
2020-02-20 21:14:42 +0100

    Use std::fs::read to read entire file

--- examples/avif2png.rs
+++ examples/avif2png.rs
@@ -1,14 +1,11 @@
 use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 
 use image::{DynamicImage, ImageOutputFormat, RgbImage};
 
 fn main() {
     let input = env::args().nth(1).expect("input filename");
-    let mut buf = vec![];
-    let mut f = File::open(&input).expect("opening");
-    f.read_to_end(&mut buf).expect("reading");
+    let buf = fs::read(&input).expect("reading");
 
     let pixels = libavif::decode_rgb(&buf).expect("decoding");
     eprintln!("w={}, h={}", pixels.width(), pixels.height());
