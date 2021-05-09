49d1542f49d04450b47e795970fba14ee89be690
Kornel <kornel@geekhood.net>
2021-05-09 12:27:05 +0100

    Update libaom

--- libaom-sys/src/lib.rs
+++ libaom-sys/src/lib.rs
@@ -1,3 +1,24 @@
 #[allow(bad_style)]
 mod ffi;
 pub use ffi::*;
+
+#[test]
+fn test_aom_binding_abi_ver() {
+    let cfg = aom_codec_dec_cfg {
+        w: 0,
+        h: 0,
+        threads: 1,
+        allow_lowbitdepth: 1,
+    };
+    let res = unsafe {
+        let mut ctx = std::mem::MaybeUninit::uninit();
+        aom_codec_dec_init_ver(
+            ctx.as_mut_ptr(),
+            aom_codec_av1_dx(),
+            &cfg,
+            0,
+            AOM_DECODER_ABI_VERSION as i32,
+        )
+    };
+    assert_eq!(0, res);
+}
