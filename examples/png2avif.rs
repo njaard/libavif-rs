d8450dcfaa35868cb2e0fdaeac77394be6a36db1
LongYinan <lynweklm@gmail.com>
2022-04-08 16:02:59 +0800

    Support more color types for libavif-image

--- examples/png2avif.rs
+++ examples/png2avif.rs
@@ -1,29 +1,63 @@
 use std::env;
-use std::io::{self, Write};
 
-use image::DynamicImage;
+use image::{buffer::ConvertBuffer, DynamicImage, ImageBuffer, Rgb};
 
-fn main() {
+fn main() -> Result<(), libavif::Error> {
     let input = env::args().nth(1).expect("input png");
     let img = image::open(&input).expect("opening");
 
     let data = match img {
         DynamicImage::ImageRgb8(img) => {
             let rgb = img.as_flat_samples();
-            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice()).expect("encoding avif")
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
         }
         DynamicImage::ImageRgba8(img) => {
             let rgb = img.as_flat_samples();
-            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice()).expect("encoding avif")
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
         }
         DynamicImage::ImageLuma8(img) => {
-            let rgb = img.as_flat_samples();
-            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice()).expect("encoding avif")
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
         }
-        _ => panic!("image type not supported"),
+        DynamicImage::ImageLumaA8(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageLuma16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageLumaA16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgb16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgba16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgb32F(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgba32F(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        _ => panic!("Unsupported Image Type"),
     };
 
-    io::stdout()
-        .write_all(data.as_slice())
-        .expect("output avif");
+    std::fs::write("out.avif", data.as_slice()).expect("output avif");
+    Ok(())
 }
--- libavif-image/src/lib.rs
+++ libavif-image/src/lib.rs
@@ -3,7 +3,7 @@
 //!
 //! Converts to and from YUV (`image` only does RGB).
 
-use image::{DynamicImage, ImageBuffer};
+use image::{buffer::ConvertBuffer, DynamicImage, ImageBuffer, Rgb};
 
 use libavif::AvifData;
 pub use libavif::{is_avif, Error};
@@ -29,7 +29,43 @@ pub fn save(img: &DynamicImage) -> Result<AvifData, Error> {
             libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
         }
         DynamicImage::ImageLuma8(img) => {
-            let rgb = img.as_flat_samples();
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageLumaA8(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageLuma16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageLumaA16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgb16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgba16(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgb32F(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
+            libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
+        }
+        DynamicImage::ImageRgba32F(img) => {
+            let image: ImageBuffer<Rgb<u8>, _> = img.convert();
+            let rgb = image.as_flat_samples();
             libavif::encode_rgb8(img.width(), img.height(), rgb.as_slice())?
         }
         _ => return Err(Error::UnsupportedImageType),
