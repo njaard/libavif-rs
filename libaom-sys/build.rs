8a2115a011f21efd6811131cd25c73562ec26cb8
LongYinan <lynweklm@gmail.com>
2022-04-18 11:28:34 +0800

    Allow to use prebuilt libaom

--- libaom-sys/build.rs
+++ libaom-sys/build.rs
@@ -5,49 +5,66 @@ fn main() {
         return;
     }
 
-    let mut aom = cmake::Config::new("vendor");
-    aom.profile(if env::var("PROFILE").expect("PROFILE") == "release" {
-        "Release"
+    if let Ok((libaom_path, libaom_include_path, libaom_pkgconfig_path)) =
+        env::var("LIB_AOM_STATIC_LIB_PATH").and_then(|libaom_path| {
+            env::var("LIB_AOM_INCLUDE_PATH").and_then(|libaom_include_path| {
+                env::var("LIB_AOM_PKG_CONFIG_PATH").map(|libaom_pkgconfig_path| {
+                    (libaom_path, libaom_include_path, libaom_pkgconfig_path)
+                })
+            })
+        })
+    {
+        // DEP_AOM_INCLUDE, DEP_AOM_PKGCONFIG variables
+        println!("cargo:include={}", libaom_include_path);
+        println!("cargo:pkgconfig={}", libaom_pkgconfig_path);
+
+        println!("cargo:rustc-link-search=native={}", libaom_path);
     } else {
-        "Debug"
-    })
-    .define("ENABLE_DOCS", "0")
-    .define("ENABLE_EXAMPLES", "0")
-    .define("ENABLE_TESTDATA", "0")
-    .define("ENABLE_TESTS", "0")
-    .define("ENABLE_TOOLS", "0")
-    .define("CMAKE_INSTALL_LIBDIR", "lib");
+        let mut aom = cmake::Config::new("vendor");
+        aom.profile(if env::var("PROFILE").expect("PROFILE") == "release" {
+            "Release"
+        } else {
+            "Debug"
+        })
+        .define("ENABLE_DOCS", "0")
+        .define("ENABLE_EXAMPLES", "0")
+        .define("ENABLE_TESTDATA", "0")
+        .define("ENABLE_TESTS", "0")
+        .define("ENABLE_TOOLS", "0")
+        .define("CMAKE_INSTALL_LIBDIR", "lib");
 
-    let host = env::var("HOST").expect("HOST");
-    let target = env::var("TARGET").expect("TARGET");
-    if host != target {
-        let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH");
-        aom.define("AOM_TARGET_CPU", target_arch);
-    }
+        let host = env::var("HOST").expect("HOST");
+        let target = env::var("TARGET").expect("TARGET");
+        if host != target {
+            let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH");
+            aom.define("AOM_TARGET_CPU", target_arch);
+        }
 
-    if env::var("LIBAVIF_CROSS_WIN32").is_ok() {
-        aom.configure_arg("-T host=x64").configure_arg("-A Win32");
-    }
+        if env::var("LIBAVIF_CROSS_WIN32").is_ok() {
+            aom.configure_arg("-T host=x64").configure_arg("-A Win32");
+        }
 
-    if target == "armv7-unknown-linux-gnueabihf" {
-        aom.define(
-            "CMAKE_TOOLCHAIN_FILE",
-            "build/cmake/toolchains/armv7-linux-gcc.cmake",
+        if target == "armv7-unknown-linux-gnueabihf" {
+            aom.define(
+                "CMAKE_TOOLCHAIN_FILE",
+                "build/cmake/toolchains/armv7-linux-gcc.cmake",
+            );
+        }
+
+        let dst = aom.build();
+
+        // DEP_AOM_INCLUDE, DEP_AOM_PKGCONFIG variables
+        println!("cargo:include={}", dst.join("include").display());
+        println!(
+            "cargo:pkgconfig={}",
+            dst.join("lib").join("pkgconfig").display()
+        );
+
+        println!(
+            "cargo:rustc-link-search=native={}",
+            dst.join("lib").display()
         );
     }
 
-    let dst = aom.build();
-
-    // DEP_AOM_INCLUDE, DEP_AOM_PKGCONFIG variables
-    println!("cargo:include={}", dst.join("include").display());
-    println!(
-        "cargo:pkgconfig={}",
-        dst.join("lib").join("pkgconfig").display()
-    );
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        dst.join("lib").display()
-    );
     println!("cargo:rustc-link-lib=static=aom");
 }
