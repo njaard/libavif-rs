73469b7a8af295bbfa021ec8f564587f4ea952de
Paolo Barbolini <paolo.barbolini@m4ss.net>
2023-09-02 11:17:48 +0200

    libaom: regenerate bindings

--- libaom-sys/bindings.h
+++ libaom-sys/bindings.h
@@ -1,4 +1,4 @@
-// bindgen bindings.h --no-prepend-enum-name --allowlist-type="(aom|av1|OBU|AOM|AV).*" --allowlist-function="(aom|av1|OBU|AOM|AV).*" --allowlist-var="(aom|av1|OBU|AOM|AV).*" --no-layout-tests --size_t-is-usize  -- -Ivendor | sed -E 's/ ?\\\\brief ?// ' | sed -E 's/doc = " ?< /doc = "/' > src/ffi.rs
+// bindgen bindings.h --no-prepend-enum-name --allowlist-type="(aom|av1|OBU|AOM|AV).*" --allowlist-function="(aom|av1|OBU|AOM|AV).*" --allowlist-var="(aom|av1|OBU|AOM|AV).*" --no-layout-tests -- -Ivendor | sed -E 's/ ?\\\\brief ?// ' | sed -E 's/doc = " ?< /doc = "/' > src/ffi.rs
 
 #include "aom/aom.h"
 #include "aom/aom_encoder.h"
